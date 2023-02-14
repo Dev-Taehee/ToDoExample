dc427776a684b7bb08ef7a370e31690c87139587
TaeheeWoo94 <wth0086@gmail.com>
2023-02-14 13:54:56 +0900

    Spring Application 추가

---
+++ src/test/java/ToDoExample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ToDoExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
